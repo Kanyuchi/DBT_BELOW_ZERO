a6de4a9b10122b5e4017af2bdfcc04d5c890463d
Kudzanaikutsanzira <shaunkudzi@gmail.com>
2024-10-16 17:42:29 +0200

    staging_flights_edits

--- models/staging/staging_flights.sql
+++ models/staging/staging_flights.sql
@@ -1,8 +1,7 @@
-{{ config(materialized='view') }}
 
 WITH flights_two_months AS (
     SELECT * 
-    FROM {{source('staging_flights', 'flights')}}
+    FROM {{source('flights', 'flights')}}
     WHERE DATE_PART('month', flight_date) IN (1, 2) 
     AND origin IN ('ORD', 'MSP', 'DTW', 'MKE', 'CLE')
     AND dest IN ('ORD', 'MSP', 'DTW', 'MKE', 'CLE')
